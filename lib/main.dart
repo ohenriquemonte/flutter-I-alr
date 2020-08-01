ea909476306cb813cecefe568d32d6d3222b95cd
Henrique Montenegro <henrique.montenegro@slcagricola.com.br>
2020-08-01 02:16:07 -0300

    02 - Implementando layout com Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,52 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      Column(
-        children: <Widget>[
-          Text(
-            'Deliver features faster',
-            textDirection: TextDirection.ltr,
-          ),
-          Text(
-            'Deliver features faster',
-            textDirection: TextDirection.ltr,
-          ),
-          Text('Craft beautiful UIs', textDirection: TextDirection.ltr),
-          Column(
-            children: <Widget>[
-              Text(
-                'Craft beautiful UIs',
-                textDirection: TextDirection.ltr,
-              ),
-            ],
-          ),
-        ], //<Widget>[]
-      ), //Column
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        body: ListaTransferencias(),
+        appBar: AppBar(
+          title: Text('Transferências'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+        ),
+      ),
+    ));
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(Transferencia(100.00, 1000)),
+        ItemTransferencia(Transferencia(200.00, 1000)),
+        ItemTransferencia(Transferencia(300.00, 1000)),
+        ItemTransferencia(Transferencia(400.00, 1000)),
+      ],
     );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
